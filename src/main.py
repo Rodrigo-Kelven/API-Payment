674e22ac4f272f2214ad89e7a665030f4bd48051
Kelven <kelvenr40@gmail.com>
2025-04-04 23:04:32 -0300

    add details of api

--- src/main.py
+++ src/main.py
@@ -2,7 +2,16 @@ from fastapi import FastAPI
 from config.config import lifespan
 from routes.all_routes import all_Rotes
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    lifespan=lifespan,
+    debug=True,
+    title="API Based with FastAPI",
+    description="API Based é um projeto open source." \
+    "Baseado na ideia de facilitar a crição de apis do absoluto," \
+    "este projeto é uma ótima opção para os que se perguntem do que é composta uma" \
+    "api excelente em termos de seguranca, performance e escalabilidade/flexibilidade.",
+    version="0.0.5"
+    )
 
 all_Rotes(app)
 
