e5c11201d21105c6fd0907d98f5dd56c109521f7
Kelven <kelvenr40@gmail.com>
2025-04-06 20:40:17 -0300

    DOCS, update in version

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
     "Baseado na ideia de facilitar a crição de apis do absoluto," \
     "este projeto é uma ótima opção para os que se perguntem do que é composta uma" \
     "api excelente em termos de seguranca, performance e escalabilidade/flexibilidade.",
-    version="0.1.0"
+    version="0.1.1"
     )
 
 
