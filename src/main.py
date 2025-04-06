9f050e4b11c3737b326208ebaf87f0690a72d7d4
Kelven <kelvenr40@gmail.com>
2025-04-06 20:33:00 -0300

    DOCS, add docs ins main

--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ from routes.all_routes import all_Rotes
 
 from core.config.config_db import Base, engine
 from core.routes.all_routes import all_routes
+from core.auth.auth import *
+from core.config.config import *
+
 
 app = FastAPI(
     lifespan=lifespan,
@@ -16,17 +19,16 @@ app = FastAPI(
     version="0.1.0"
     )
 
-all_Rotes(app)
 
+# inicia todas as rotas implementadas nesta api
+all_Rotes(app)
+# incia as rotas de authenticacao
 all_routes(app)
 
-# tem que ficar no main.py, porque ao ser iniciado, as tabelas no db seram criadas imediatamente
+# tem que ficar no main porque ao ser iniciado, as tabelas no db seram criadas imediatamente
 print("Criando tabelas no banco de dados...")
 Base.metadata.create_all(bind=engine)
 print("Tabelas criadas.")
-
-from core.auth.auth import *
-from core.config.config import *
 
 
 # Adiciona o middleware ao FastAPI
@@ -35,6 +37,7 @@ app.add_middleware(LogRequestMiddleware)
 # Adiciona o middleware de tratamento de exceções
 app.add_middleware(ExceptionHandlingMiddleware)
 
+# funcao CORS
 config_CORS(app)
 
 if __name__ == "__main__":
